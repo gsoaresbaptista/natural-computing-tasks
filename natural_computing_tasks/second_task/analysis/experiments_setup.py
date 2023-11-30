82741ac2ce751121563c6e92fc81b3b49c05361c
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-30 00:34:14 -0300

    🐛 fix: adjust dataset split

--- natural_computing_tasks/second_task/analysis/experiments_setup.py
+++ natural_computing_tasks/second_task/analysis/experiments_setup.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from evolutionary_programming.data_processing import (
     fetch_csv_to_numpy,
-    split_train_test,
 )
 from evolutionary_programming.neural_network import (
     DenseLayer,
@@ -10,20 +9,21 @@ from evolutionary_programming.neural_network import (
     encode_neural_network,
 )
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 iris_url = (
-    'https://raw.githubusercontent.com/gsoaresbaptista/'
-    'datasets/main/datasets/iris.csv'
+    "https://raw.githubusercontent.com/gsoaresbaptista/"
+    "datasets/main/datasets/iris.csv"
 )
 
 hepatitis_url = (
-    'https://raw.githubusercontent.com/gsoaresbaptista/'
-    'datasets/main/datasets/hepatitis.csv'
+    "https://raw.githubusercontent.com/gsoaresbaptista/"
+    "datasets/main/datasets/hepatitis.csv"
 )
 
 heart_url = (
-    'https://raw.githubusercontent.com/gsoaresbaptista/'
-    'datasets/main/datasets/heart_disease.csv'
+    "https://raw.githubusercontent.com/gsoaresbaptista/"
+    "datasets/main/datasets/heart_disease.csv"
 )
 
 
@@ -35,20 +35,20 @@ class NeuralNetworkArchitectures:
     @staticmethod
     def iris_architecture() -> NeuralNetwork:
         module = NeuralNetwork(
-            1e-3, loss_function='softmax_neg_log_likelihood', momentum=0.9
+            1e-3, loss_function="softmax_neg_log_likelihood", momentum=0.9
         )
         module.add_layer(
             [
                 DenseLayer(
                     4,
                     10,
-                    'tanh',
+                    "tanh",
                     regularization_strength=PREDICTION_REGULARIZATION,
                 ),
                 DenseLayer(
                     10,
                     3,
-                    'linear',
+                    "linear",
                     regularization_strength=PREDICTION_REGULARIZATION,
                 ),
             ]
@@ -58,20 +58,20 @@ class NeuralNetworkArchitectures:
     @staticmethod
     def hepatitis_architecture() -> NeuralNetwork:
         module = NeuralNetwork(
-            1e-3, loss_function='softmax_neg_log_likelihood', momentum=0.9
+            1e-3, loss_function="softmax_neg_log_likelihood", momentum=0.9
         )
         module.add_layer(
             [
                 DenseLayer(
                     16,
                     50,
-                    'tanh',
+                    "tanh",
                     regularization_strength=PREDICTION_REGULARIZATION,
                 ),
                 DenseLayer(
                     50,
                     2,
-                    'linear',
+                    "linear",
                     regularization_strength=PREDICTION_REGULARIZATION,
                 ),
             ]
@@ -81,20 +81,20 @@ class NeuralNetworkArchitectures:
     @staticmethod
     def heart_architecture() -> NeuralNetwork:
         module = NeuralNetwork(
-            1e-3, loss_function='softmax_neg_log_likelihood', momentum=0.9
+            1e-3, loss_function="softmax_neg_log_likelihood", momentum=0.9
         )
         module.add_layer(
             [
                 DenseLayer(
                     13,
                     50,
-                    'tanh',
+                    "tanh",
                     regularization_strength=PREDICTION_REGULARIZATION,
                 ),
                 DenseLayer(
                     50,
                     2,
-                    'linear',
+                    "linear",
                     regularization_strength=PREDICTION_REGULARIZATION,
                 ),
             ]
@@ -105,9 +105,7 @@ class NeuralNetworkArchitectures:
 class DecodeGuides:
     @staticmethod
     def iris_guide() -> list:
-        _, guide = encode_neural_network(
-            NeuralNetworkArchitectures.iris_architecture()
-        )
+        _, guide = encode_neural_network(NeuralNetworkArchitectures.iris_architecture())
         return guide
 
     @staticmethod
@@ -136,8 +134,8 @@ class DatasetsDownloader:
         x = np.concatenate(data[:-1], axis=1)
         y = one_hot.fit_transform(data[-1].reshape(-1, 1))
 
-        ((x_train, y_train), (x_test, y_test)) = split_train_test(
-            x, y, train_percentage=0.8, sequential=False
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, train_size=0.8, random_state=42, stratify=y
         )
 
         # preprocess data
@@ -146,15 +144,9 @@ class DatasetsDownloader:
         x_train = scaler.transform(x_train)
         x_test = scaler.transform(x_test)
 
-        size, rnd = len(x_train), np.random.RandomState(0)
-        indices = rnd.choice(range(size), size, replace=False)
-        x_train = x_train[indices]
-        y_train = y_train[indices]
-
         return {
-            'indices': np.argsort(indices),
-            'processed': ((x_train, y_train), (x_test, y_test)),
-            'inverse_fn': lambda x: scaler.inverse_transform(x),
+            "processed": ((x_train, y_train), (x_test, y_test)),
+            "inverse_fn": lambda x: scaler.inverse_transform(x),
         }
 
     @staticmethod
@@ -166,11 +158,11 @@ class DatasetsDownloader:
         df = pd.DataFrame(data.T, columns=columns)
 
         # imputing data
-        df = df.replace('', np.nan)
-        df = df.drop(['protime', 'alk_phosphate', 'albumin'], axis=1)
+        df = df.replace("", np.nan)
+        df = df.drop(["protime", "alk_phosphate", "albumin"], axis=1)
         df = df.dropna()
-        df = df.astype('string')
-        df = df.astype({'age': 'float', 'bilirubin': 'float', 'sgot': float})
+        df = df.astype("string")
+        df = df.astype({"age": "float", "bilirubin": "float", "sgot": float})
 
         # encoder values
         columns_to_encoder = df.columns[(df.map(type) == str).all(0)]
@@ -183,9 +175,8 @@ class DatasetsDownloader:
         x = df.iloc[:, 1:-1].values
         y = one_hot.fit_transform(df.iloc[:, -1].values.reshape(-1, 1))
 
-        # split data
-        ((x_train, y_train), (x_test, y_test)) = split_train_test(
-            x, y, train_percentage=0.8, sequential=False
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, train_size=0.8, random_state=42, stratify=y
         )
 
         # preprocess data
@@ -194,15 +185,9 @@ class DatasetsDownloader:
         x_train = scaler.transform(x_train)
         x_test = scaler.transform(x_test)
 
-        size, rnd = len(x_train), np.random.RandomState(0)
-        indices = rnd.choice(range(size), size, replace=False)
-        x_train = x_train[indices]
-        y_train = y_train[indices]
-
         return {
-            'indices': np.argsort(indices),
-            'processed': ((x_train, y_train), (x_test, y_test)),
-            'inverse_fn': lambda x: scaler.inverse_transform(x),
+            "processed": ((x_train, y_train), (x_test, y_test)),
+            "inverse_fn": lambda x: scaler.inverse_transform(x),
         }
 
     @staticmethod
@@ -217,9 +202,8 @@ class DatasetsDownloader:
         x = df.iloc[:, :-1].values
         y = one_hot.fit_transform(df.iloc[:, -1].values.reshape(-1, 1))
 
-        # split data
-        ((x_train, y_train), (x_test, y_test)) = split_train_test(
-            x, y, train_percentage=0.8, sequential=False
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, train_size=0.8, random_state=42, stratify=y
         )
 
         # preprocess data
@@ -228,13 +212,7 @@ class DatasetsDownloader:
         x_train = scaler.transform(x_train)
         x_test = scaler.transform(x_test)
 
-        size, rnd = len(x_train), np.random.RandomState(0)
-        indices = rnd.choice(range(size), size, replace=False)
-        x_train = x_train[indices]
-        y_train = y_train[indices]
-
         return {
-            'indices': np.argsort(indices),
-            'processed': ((x_train, y_train), (x_test, y_test)),
-            'inverse_fn': lambda x: scaler.inverse_transform(x),
+            "processed": ((x_train, y_train), (x_test, y_test)),
+            "inverse_fn": lambda x: scaler.inverse_transform(x),
         }
